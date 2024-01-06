2825f60168fdc595b3521a689063222ab5209206
karkiayush <karkiayush619@gmail.com>
2024-01-06 18:54:17 +0545

    working on adapter

--- app/src/main/java/com/example/notesapp/NotesAdapter.kt
+++ app/src/main/java/com/example/notesapp/NotesAdapter.kt
@@ -1,9 +1,54 @@
 package com.example.notesapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+
 /**
  * Here we'll be creating:
  * 1. ViewHolder
  * 2. Recycler adapter and its 3 method
  * 3. Lastly a refresh data function
  * */
-class NotesAdapter {
+class NotesAdapter(
+    private var notes: List<NoteContent>,
+    /**Here the context represents the notes adpater*/
+    context: Context
+) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.noteTitleId)
+        val contentTextView: TextView = itemView.findViewById(R.id.noteContentId)
+
+
+    }
+
+    /** In the onCreateViewHolder, we set up the item layout view*/
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.eachrow, parent, false)
+        return NoteViewHolder(view)
+    }
+
+
+    /** Here in onBindViewHolder, we bind data on the element*/
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.titleTextView.text = note.title
+        holder.contentTextView.text = note.noteDescription
+    }
+
+
+    /** The size is returned*/
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    fun refreshData() {
+
+    }
+
 }
